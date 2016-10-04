bd87a62a8309eeb174b77e1740212ba34aed340c
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2016-10-04 13:46:26 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+if(file_exists("config.php")){
+	include("config.php");
+}else{
+	header("Location: install.php");
+}
+?>
+<!DOCTYPE html> 
+<html lang="it"> 
+	<head> 
+		<meta charset=utf-8> 
+		<title><?php echo $title; ?></title>
+		<style>
+		<?php include 'style.css'; ?>
+		</style>
+		
+	</head>
+<body>
+	<div class="titolo">
+		Gestione Magazzino 
+		<?php include("menu.php"); ?>
+	</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,26 @@
+
+<acronym title="Carico">
+	<a href="carico.php">
+		<img src="img/view.png" alt="Carico" />
+	</a>
+</acronym>
+<acronym title="Scarico">
+	<a href="scarico.php">
+		<img src="img/new.png" alt="Scarico" />
+	</a>
+</acronym>
+<acronym title="Ricerca">
+	<a href="search.php" >
+		<img src="img/search.png" alt="Ricerca" />
+	</a>
+</acronym>
+<acronym title="Movimenti">
+	<a href="movimenti.php" >
+		<img src="img/stat.png" alt="movimenti" />
+	</a>
+</acronym>
+<acronym title="Impostazioni">
+	<a href="admin.php" >
+		<img src="img/setting.png" alt="Impostazioni" />
+	</a>
+</acronym>
