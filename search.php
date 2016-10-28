de905ce3704150445d6632d3844e6d757f927000
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2016-10-28 11:49:28 +0200

    c'era un bug, un [0] al posto di [0]

--- search.php
+++ search.php
@@ -68,7 +68,7 @@
                 $rs = mysql_fetch_row($query);
 				$id=$rs[7];
 				echo "<tr><td>$rs[1]</td><td>$rs[2]</td><td>$rs[3]</td><td>$rs[4]</td><td>$rs[6]</td>";
-				echo "<td>      <a href=\"del.php?id=$row[0]\"><img src=\"img/del_small.png\" alt=\"scarica\" /></a>
+				echo "<td>      <a href=\"del.php?id=$rs[0]\"><img src=\"img/del_small.png\" alt=\"scarica\" /></a>
                                 <a href=\"add.php?id=$row[0]\"><img src=\"img/add_small.png\" alt=\"carica\" /></a>";
 		                if($rs[4] < 5){
                 		        echo "<img src=\"img/warning.png\" alt=\"warning\" />";
