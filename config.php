92d3e228b156ec07391d67291c68ee8327f2afd0
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2016-11-11 12:22:49 +0100

    Revert "boh"
    This reverts commit d4feef8c019ef79ab72cbab74e1970162cf06dcc.

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // persolizza i tuoi parametri
 $title = "Magazzinus";			// il titolo che comparirà sulla barra del browser
 $hostname = "localhost";		// l'host del tuo database
-$username = "USERNAME";		// il nome utente per accedere al database
-$password = "PASSWORD";	// la password per accedere al database
+$username = "root";		// il nome utente per accedere al database
+$password = "cf81003";	// la password per accedere al database
 $database = "magazzinus";	// il nome del tuo database
 
 // non toccare niente qua sotto
